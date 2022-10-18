286c7a9275638e4963df8cf988d320a0a5d7194c
chenhongtao <chenhongtao@uniontech.com>
2022-10-18 17:52:52 +0800

    feat: add log
    Log: log finished

--- src/filechooser.cpp
+++ src/filechooser.cpp
@@ -1,5 +1,8 @@
 #include "filechooser.h"
+#include <QLoggingCategory>
+#include <qloggingcategory.h>
 
+Q_LOGGING_CATEGORY(XdgDesktopDDEFileChooser, "xdg-dde-filechooser")
 FileChooserProtal::FileChooserProtal(QObject *parent)
     : QDBusAbstractAdaptor(parent)
 {
@@ -12,6 +15,7 @@ uint FileChooserProtal::OpenFile(const QDBusObjectPath &handle,
                                  const QVariantMap &options,
                                  QVariantMap &results)
 {
+    qCDebug(XdgDesktopDDEFileChooser) << "start OpenFile";
     return 1;
 }
 uint FileChooserProtal::SaveFile(const QDBusObjectPath &handle,
@@ -21,5 +25,6 @@ uint FileChooserProtal::SaveFile(const QDBusObjectPath &handle,
                                  const QVariantMap &options,
                                  QVariantMap &results)
 {
+    qCDebug(XdgDesktopDDEFileChooser) << "start SaveFile";
     return 1;
 }
--- src/notification.cpp
+++ src/notification.cpp
@@ -1,10 +1,19 @@
 #include "notification.h"
+#include <QLoggingCategory>
+
+Q_LOGGING_CATEGORY(XdgDesktopDDENotification, "xdg-dde-notification")
 
 NotificationProtal::NotificationProtal(QObject *parent)
     : QDBusAbstractAdaptor(parent)
 {
 }
 
-void NotificationProtal::AddNotification(const QString &app_id, const QString &id, const QVariantMap &notification) {}
+void NotificationProtal::AddNotification(const QString &app_id, const QString &id, const QVariantMap &notification)
+{
+    qCDebug(XdgDesktopDDENotification) << "notification add start";
+}
 
-void NotificationProtal::RemoveNotification(const QString &app_id, const QString &id) {}
+void NotificationProtal::RemoveNotification(const QString &app_id, const QString &id)
+{
+    qCDebug(XdgDesktopDDENotification) << "notification remove start";
+}
