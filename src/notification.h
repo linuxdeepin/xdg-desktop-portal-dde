9de48e1aab30d5252303dbd887d7fb0f7937933b
zhaoyingzhen <zhaoyingzhen@uniontech.com>
2022-12-08 18:05:45 +0800

    feat: add notification service
    add notification service
    
    Log:
    Influence: Notification.
    Change-Id: I2ed3e493eb793ab6bd28ea55d0902c80f79baa72

--- src/notification.h
+++ src/notification.h
@@ -7,6 +7,8 @@
 #include <QDBusAbstractAdaptor>
 #include <QDBusObjectPath>
 
+class NotificationService;
+
 class NotificationPortal : public QDBusAbstractAdaptor
 {
     Q_OBJECT
@@ -16,10 +18,24 @@ public:
     explicit NotificationPortal(QObject *parent);
     ~NotificationPortal() = default;
 
-signals:
-    void ActionInvoked(const QString &, const QString &, const QString &, const QList<QVariant> &) const;
-
 public slots:
     void AddNotification(const QString &app_id, const QString &id, const QVariantMap &notification);
     void RemoveNotification(const QString &app_id, const QString &id);
+
+private slots:
+    void actionInvoked(uint nId, const QString &action);
+    void clearTimeoutData();
+
+private:
+    struct NotificationInfo {
+        QString appId;
+        QString id; // from "AddNotification" param2
+        qint64 timestamp;
+    };
+
+private:
+    NotificationService *m_service;
+
+    QMap<uint, NotificationInfo> m_idInfoMap;
+    QMap<QString, uint> m_idMaps;
 };
