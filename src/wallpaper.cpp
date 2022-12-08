ecba33ecd11a259de6b3aa26c4adec0037223ca9
caixiangrong <caixiangrong@uniontech.com>
2022-12-08 16:08:51 +0800

    feat: realize the wallpaper
    Realize the wallpaper
    
    Log: Realize the wallpaper

--- src/wallpaper.cpp
+++ src/wallpaper.cpp
@@ -24,12 +24,27 @@ uint WallPaperPortal::SetWallpaperURI(const QDBusObjectPath &handle,
                                       const QString &uri,
                                       const QVariantMap &options)
 {
+    // TODO: 未处理options，仅实现设置主屏壁纸
+    // options.value("show-preview").toBool(); // whether to show a preview of the picture. Note that the portal may decide to show a preview even if this option is not set
+    // options.value("set-on").toString(); // where to set the wallpaper. Possible values are 'background', 'lockscreen' or 'both'
+
     qCDebug(XdgDesktopDDEWallpaper) << "Start set wallpaper";
-    QDBusMessage msg = QDBusMessage::createMethodCall(QStringLiteral("org.deepin.daemon.Appearance"),
-                                                      QStringLiteral("/org/deepin/daemon/Appearance"),
-                                                      QStringLiteral("com.deepin.daemon.Appearance"),
+    QDBusMessage primaryMsg = QDBusMessage::createMethodCall(QStringLiteral("org.deepin.dde.Display1"),
+                                                             QStringLiteral("/org/deepin/dde/Display1"),
+                                                             QStringLiteral("org.freedesktop.DBus.Properties"),
+                                                             QStringLiteral("Get"));
+    primaryMsg.setArguments({QVariant::fromValue(QStringLiteral("org.deepin.dde.Display1")), QVariant::fromValue(QStringLiteral("Primary"))});
+    QDBusPendingReply<QDBusVariant> primaryReply = QDBusConnection::sessionBus().call(primaryMsg);
+    if (primaryReply.isError()) {
+        qCDebug(XdgDesktopDDEWallpaper) << "setting failed";
+        return 1;
+    }
+
+    QDBusMessage msg = QDBusMessage::createMethodCall(QStringLiteral("org.deepin.dde.Appearance1"),
+                                                      QStringLiteral("/org/deepin/dde/Appearance1"),
+                                                      QStringLiteral("org.deepin.dde.Appearance1"),
                                                       QStringLiteral("SetMonitorBackground"));
-    msg.setArguments({QVariant::fromValue(parent_window), QVariant::fromValue(uri)});
+    msg.setArguments({QVariant::fromValue(primaryReply.value().variant().toString()), QVariant::fromValue(uri)});
     QDBusPendingReply<> pcall = QDBusConnection::sessionBus().call(msg);
     if (pcall.isValid()) {
         qCDebug(XdgDesktopDDEWallpaper) << "setting succeed";
