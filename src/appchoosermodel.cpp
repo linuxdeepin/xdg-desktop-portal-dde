7b2e62da5f532f9fc09efa77014ba1200d5949ea
linxin <linxin@deepin.org>
2023-08-30 16:46:31 +0800

    feat: modify AppChooser module
    modify AppChooser module to get data from refacted dde-application-manager
    
    Log: modify interface invoke

--- src/appchoosermodel.cpp
+++ src/appchoosermodel.cpp
@@ -12,6 +12,9 @@
 AppChooserModel::AppChooserModel(QObject *parent)
     :QAbstractListModel(parent)
 {
+    qDBusRegisterMetaType<ObjectInterfaceMap>();
+    qDBusRegisterMetaType<ObjectMap>();
+    qDBusRegisterMetaType<PropMap>();
     QMetaObject::invokeMethod(this, &AppChooserModel::loadApplications, Qt::QueuedConnection);
 }
 
@@ -24,10 +27,10 @@ void AppChooserModel::click(const QModelIndex &index)
     }
 }
 
-void AppChooserModel::click(const QString &desktop)
+void AppChooserModel::click(const QString &appId)
 {
     for (auto data : m_datas) {
-        if (data.desktop == desktop) {
+        if (data.appId == appId) {
             data.selected = true;
             break;
         }
@@ -39,7 +42,7 @@ QStringList AppChooserModel::choices()
     QStringList list;
     for (auto data : m_datas) {
         if (data.selected)
-            list.append(data.desktop);
+            list.append(data.appId);
     }
     return list;
 }
@@ -49,28 +52,36 @@ void AppChooserModel::loadApplications()
     ItemInfo::registerMetaType();
 
 #ifdef QT_DEBUG
-    QDBusPendingCall call = QDBusInterface("com.deepin.dde.daemon.Launcher", "/com/deepin/dde/daemon/Launcher", "com.deepin.dde.daemon.Launcher", QDBusConnection::sessionBus()).asyncCall(QString("GetAllItemInfos"));
+    QDBusPendingCall call = QDBusInterface("org.desktopspec.ApplicationManager1", "/org/desktopspec/ApplicationManager1", "org.desktopspec.DBus.ObjectManager", QDBusConnection::sessionBus()).asyncCall(QString("GetManagedObjects"));
 #else
-    QDBusPendingCall call = QDBusInterface("org.deepin.dde.daemon.Launcher1", "/org/deepin/dde/daemon/Launcher1", "org.deepin.dde.daemon.Launcher1", QDBusConnection::sessionBus()).asyncCall(QString("GetAllItemInfos"));
+    QDBusPendingCall call = QDBusInterface("org.desktopspec.ApplicationManager1", "/org/desktopspec/ApplicationManager1", "org.desktopspec.DBus.ObjectManager", QDBusConnection::sessionBus()).asyncCall(QString("GetManagedObjects"));
 #endif
     QDBusPendingCallWatcher *watcher = new QDBusPendingCallWatcher(call, this);
     QObject::connect(watcher, &QDBusPendingCallWatcher::finished, this, [this](QDBusPendingCallWatcher* call){
-        QDBusPendingReply<ItemInfoList> reply = *call;
+        QDBusPendingReply<ObjectMap> reply = *call;
         if (!reply.isError()) {
             m_datas.clear();
-            const auto itemList = reply.value();
-            for (auto item : itemList) {
-                QSettings settings(item.m_desktop, QSettings::IniFormat);
-                settings.beginGroup("Desktop Entry");
-                if (!settings.contains("MimeType"))
-                    continue;
-
+            const ObjectMap objectMap = reply.value();
+            const auto language = getenv("LANGUAGE");
+            for (auto obj = objectMap.begin(); obj != objectMap.end(); obj++)
+            {
+                const ObjectInterfaceMap interfaceMap = obj.value();
+                const auto infoMap = interfaceMap.begin().value();
                 DesktopInfo info;
-                info.desktop = item.m_desktop;
-                info.name = item.m_name;
-                info.icon = item.m_iconKey;
+                info.appId = infoMap["ID"].toString();
+                const auto nameMap = infoMap["DisplayName"].value<QDBusArgument>();
+                PropMap propName;
+                nameMap >> propName;
+                if (propName["Name"].contains(language)) {
+                    info.name = propName["Name"][language];
+                } else {
+                    info.name = propName["Name"]["default"];
+                }
+                const auto iconMap = infoMap["IconName"].value<QDBusArgument>();
+                PropMap propIcon;
+                iconMap >> propIcon;
+                info.icon = propIcon["default"]["default"];
                 info.selected = false;
-
                 beginInsertRows(QModelIndex(), m_datas.size(), m_datas.size());
                 m_datas.append(info);
                 endInsertRows();
@@ -90,7 +101,7 @@ QVariant AppChooserModel::data(const QModelIndex &index, int role) const
     const DesktopInfo &info = m_datas.at(index.row());
     switch (role) {
     case AppChooserModel::DataRole:
-        return info.desktop;
+        return info.appId;
     case AppChooserModel::IconRole:
         return info.icon;
     case AppChooserModel::NameRole:
@@ -101,3 +112,18 @@ QVariant AppChooserModel::data(const QModelIndex &index, int role) const
         return QVariant();
     }
 }
+
+const QDBusArgument &operator>>(const QDBusArgument &argument, PropMap &dict) {
+    argument.beginMap();
+    while (!argument.atEnd()) {
+        QString arg;
+        QMap<QString, QString> argMap;
+        argument.beginMapEntry();
+        argument >> arg >> argMap;
+        argument.endMapEntry();
+        dict.insert(arg, argMap);
+    }
+    argument.endMap();
+    return argument;
+}
+
