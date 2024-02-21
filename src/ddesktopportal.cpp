b0d904f956aaa0d03b51e5ed10f35476916deccc
chenhongtao <chenhongtao@deepin.org>
2024-02-21 14:49:15 +0800

    chore: delete things about remote and kde
    it cannot be used on qt6, and they do not work
    
    Log:

--- src/ddesktopportal.cpp
+++ src/ddesktopportal.cpp
@@ -8,18 +8,14 @@
 #include "secret.h"
 #include "lockdown.h"
 #include "globalshortcut.h"
-#include "session.h"
-#include "remotedesktop.h"
 #include "account.h"
 #include "inhibit.h"
 #include "settings.h"
 #include "screenshot.h"
-#include "screencast.h"
 #include "background.h"
 #include "filechooser.h"
 #include "wallpaper.h"
 #include "notification.h"
-#include "waylandintegration.h"
 
 DDesktopPortal::DDesktopPortal(QObject *parent)
     : QObject(parent)
@@ -31,16 +27,12 @@ DDesktopPortal::DDesktopPortal(QObject *parent)
     const QByteArray &xdgCurrentDesktop = qgetenv("XDG_CURRENT_DESKTOP").toUpper();
     if (xdgCurrentDesktop == "DDE" || xdgCurrentDesktop == "DEEPIN") {
         m_screenshot = new ScreenshotPortal(this);
-        m_screencast = new ScreenCastPortal(this);
         m_background = new BackgroundPortal(this);
         m_settings = new SettingsPortal(this);
         m_inhibit = new InhibitPortal(this);
         m_account = new AccountPortal(this);
-        m_session = new SessionPortal(this);
-        m_remote = new RemoteDesktopPortal(this);
         m_shortcut = new GlobalShortcutPortal(this);
         m_lockdown = new LockdownPortal(this);
         m_secret = new SecretPortal(this);
-        WaylandIntegration::init();
     }
 }
