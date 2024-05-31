61e5bc6b02ffbec9e68e1a3e2ec9eaa3a8c5260b
Yixue Wang <wangyixue@deepin.org>
2024-05-31 15:23:58 +0800

    feat: add portal wayland module
     * Add portal wayland context.
     * Implement Screenshot.Screenshot.
    
    Log: add portal wayland module

--- src/xdg-desktop-portal-dde.cpp
+++ src/xdg-desktop-portal-dde.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
 #include "ddesktopportal.h"
+#include "wayland/portalwaylandcontext.h"
 
 #include <QApplication>
 #include <QDBusConnection>
@@ -10,6 +11,11 @@
 #include <QLoggingCategory>
 
 Q_LOGGING_CATEGORY(XdgDesktopDDE, "xdg-dde")
+
+inline static bool onWayland()
+{
+    return QGuiApplication::platformName() == "wayland";
+}
 
 int main(int argc, char *argv[])
 {
@@ -29,15 +35,27 @@ int main(int argc, char *argv[])
 
     QDBusConnection sessionBus = QDBusConnection::sessionBus();
     if (sessionBus.registerService(QStringLiteral("org.freedesktop.impl.portal.desktop.dde"))) {
-        DDesktopPortal *desktopPortal = new DDesktopPortal(&a);
-        if (sessionBus.registerObject(
-                QStringLiteral("/org/freedesktop/portal/desktop"), desktopPortal, QDBusConnection::ExportAdaptors)) {
-            qCDebug(XdgDesktopDDE) << "portal started";
+        if (onWayland()) {
+            PortalWaylandContext *waylandContext = new PortalWaylandContext(&a);
+            if (sessionBus.registerObject(QStringLiteral("/org/freedesktop/portal/desktop"),
+                                          waylandContext,
+                                          QDBusConnection::ExportAdaptors)) {
+                qCDebug(XdgDesktopDDE) << "portal started on wayland";
+            }
+        } else {
+            DDesktopPortal *desktopPortal = new DDesktopPortal(&a);
+            if (sessionBus.registerObject(QStringLiteral("/org/freedesktop/portal/desktop"),
+                                          desktopPortal,
+                                          QDBusConnection::ExportAdaptors)) {
+                qCDebug(XdgDesktopDDE) << "portal started on x11";
+            }
         }
+
     } else {
         qCDebug(XdgDesktopDDE) << "Another portal is starting";
         return 1;
     }
+
 
     return a.exec();
 }
