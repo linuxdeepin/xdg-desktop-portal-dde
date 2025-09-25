74e80d5499c9e43abfe79517d7cbfe9f723c2ea0
YaoBing Xiao <xiaoyaobing@uniontech.com>
2025-09-25 19:05:26 +0800

    feat: remove useless function
    Log:

--- src/wayland/screencastcontext.cpp
+++ src/wayland/screencastcontext.cpp
@@ -10,42 +10,8 @@
 #include <drm_fourcc.h>
 #include <fcntl.h>
 #include <sys/mman.h>
-#include <sys/param.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <sys/timerfd.h>
 
 #include <QSocketNotifier>
-
-static void randname(char *buf) {
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
-    long r = ts.tv_nsec;
-    for (int i = 0; i < 6; ++i) {
-        assert(buf[i] == 'X');
-        buf[i] = 'A'+(r&15)+(r&16)*2;
-        r >>= 5;
-    }
-}
-
-static int anonymous_shm_open(void) {
-    char name[] = "/xdpw-shm-XXXXXX";
-    int retries = 100;
-
-    do {
-        randname(name + strlen(name) - 6);
-
-        --retries;
-        // shm_open guarantees that O_CLOEXEC is set
-        int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-        if (fd >= 0) {
-            shm_unlink(name);
-            return fd;
-        }
-    } while (retries > 0 && errno == EEXIST);
-
-    return -1;
-}
 
 static void on_core_error(void *data, uint32_t id, int seq, int res, const char* message) {
     qCFatal(SCREENCAST, "pipewire: fatal error event from core");
