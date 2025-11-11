7e8bc87a529942810da970a9aac26adca58e42c1
YaoBing Xiao <xiaoyaobing@uniontech.com>
2025-11-11 15:14:30 +0800

    feat: update zh_CN translation
    Log:

--- misc/translations/translations/xdg-desktop-portal-dde_zh_CN.ts
+++ misc/translations/translations/xdg-desktop-portal-dde_zh_CN.ts
@@ -1,13 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="zh_CN">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>AccessDialog</name>
     <message>
-        <location filename="../../../src/accessdialog.cpp" line="51"/>
+        <location filename="../../../src/accessdialog.cpp" line="46"/>
         <source>Deny Access</source>
         <translation>拒绝</translation>
     </message>
     <message>
-        <location filename="../../../src/accessdialog.cpp" line="59"/>
+        <location filename="../../../src/accessdialog.cpp" line="54"/>
         <source>Grant Access</source>
         <translation>授权</translation>
     </message>
@@ -23,6 +25,57 @@
         <location filename="../../../src/appchooserdialog.cpp" line="19"/>
         <source>Confirm</source>
         <translation>确认</translation>
+    </message>
+</context>
+<context>
+    <name>ScreencastChooserWindow</name>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="42"/>
+        <source>Application [%1] requests to share you screen content</source>
+        <translation>应用程序[%1]请求共享您的屏幕内容</translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="48"/>
+        <source>Please select the screen or window you wish to share</source>
+        <translation>请选择您要共享的屏幕或窗口</translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="56"/>
+        <source>Screen</source>
+        <translation>屏幕</translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="62"/>
+        <source>Window</source>
+        <translation>窗口</translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="121"/>
+        <source>Allow restoring on future sessions</source>
+        <translation>允许在未来会话中恢复</translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="129"/>
+        <source>Accept</source>
+        <translation>允许</translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="133"/>
+        <source>Reject</source>
+        <translation>拒绝</translation>
+    </message>
+</context>
+<context>
+    <name>ScreencastPortalWayland</name>
+    <message>
+        <location filename="../../../src/wayland/screencastportal.cpp" line="63"/>
+        <source>Sharing Screen to [%1]</source>
+        <translation>正在共享屏幕至[%1]</translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/screencastportal.cpp" line="64"/>
+        <source>Stop [%1] Sharing</source>
+        <translation>停止[%1]共享</translation>
     </message>
 </context>
 </TS>
