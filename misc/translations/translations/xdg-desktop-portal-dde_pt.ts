bbd5d8736dfd52045aa1abd6a1100cec9d0d928f
zhangkun <zhangkun2@uniontech.com>
2025-11-13 11:37:54 +0800

    chore(i18n): update ts
    as title
    
    Log:

--- misc/translations/translations/xdg-desktop-portal-dde_pt.ts
+++ misc/translations/translations/xdg-desktop-portal-dde_pt.ts
@@ -1,13 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="pt">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt">
 <context>
     <name>AccessDialog</name>
     <message>
-        <location filename="../../../src/accessdialog.cpp" line="51"/>
+        <location filename="../../../src/accessdialog.cpp" line="46"/>
         <source>Deny Access</source>
         <translation>Negar Acesso</translation>
     </message>
     <message>
-        <location filename="../../../src/accessdialog.cpp" line="59"/>
+        <location filename="../../../src/accessdialog.cpp" line="54"/>
         <source>Grant Access</source>
         <translation>Permitir Acesso</translation>
     </message>
@@ -23,6 +25,57 @@
         <location filename="../../../src/appchooserdialog.cpp" line="19"/>
         <source>Confirm</source>
         <translation>Confirmar</translation>
+    </message>
+</context>
+<context>
+    <name>ScreencastChooserWindow</name>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="42"/>
+        <source>Application [%1] requests to share you screen content</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="48"/>
+        <source>Please select the screen or window you wish to share</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="56"/>
+        <source>Screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="62"/>
+        <source>Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="123"/>
+        <source>Allow restoring on future sessions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="131"/>
+        <source>Accept</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/ScreencastChooserWindow.qml" line="137"/>
+        <source>Reject</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ScreencastPortalWayland</name>
+    <message>
+        <location filename="../../../src/wayland/screencastportal.cpp" line="63"/>
+        <source>Sharing Screen to [%1]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../src/wayland/screencastportal.cpp" line="64"/>
+        <source>Stop [%1] Sharing</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
